dc45fefa7fdecc424458accbe03e67c3b91334f7
ufuk <ufuk-dundar@outlook.com>
2016-12-28 23:06:34 +0300

    Updated.

--- Contacts/Form1.Designer.cs
+++ Contacts/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtCalls = new System.Windows.Forms.TextBox();
+            this.lstFound = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCalls
+            // 
+            this.txtCalls.Location = new System.Drawing.Point(55, 47);
+            this.txtCalls.Name = "txtCalls";
+            this.txtCalls.Size = new System.Drawing.Size(245, 22);
+            this.txtCalls.TabIndex = 0;
+            this.txtCalls.TextChanged += new System.EventHandler(this.txtCalls_TextChanged);
+            // 
+            // lstFound
+            // 
+            this.lstFound.FormattingEnabled = true;
+            this.lstFound.ItemHeight = 16;
+            this.lstFound.Location = new System.Drawing.Point(55, 104);
+            this.lstFound.Name = "lstFound";
+            this.lstFound.Size = new System.Drawing.Size(245, 292);
+            this.lstFound.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(50, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Calls";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(365, 412);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstFound);
+            this.Controls.Add(this.txtCalls);
+            this.Name = "Form1";
+            this.Text = "Contacts";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtCalls;
+        private System.Windows.Forms.ListBox lstFound;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Contacts/Form1.cs
+++ Contacts/Form1.cs
@@ -16,5 +16,43 @@ namespace Contacts
         {
             InitializeComponent();
         }
+        List<string> contactList = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            contactList.Add("Ufuk");
+            contactList.Add("Ali");
+            contactList.Add("Zeynep");
+            contactList.Add("Ayşe");
+            contactList.Add("Sema");
+            contactList.Add("Fatma");
+            contactList.Add("Said");
+            contactList.Add("Mustafa");
+            contactList.Add("Kamil");
+            contactList.Add("Yiğit");
+            contactList.Add("Cansu");
+            contactList.Add("Elif");
+            contactList.Add("Beste");
+            contactList.Add("Büşra");
+            contactList.Add("Ece");
+            contactList.Add("Emre");
+            contactList.Add("Umut");
+
+        }
+
+        private void txtCalls_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCalls.Text != "")
+            {
+                lstFound.Items.Clear();
+                foreach(string name in contactList)
+                {
+                    //if (name.ToLower().StartsWith(txtCalls.Text.ToLower()))
+                    if(name.ToLower().Contains(txtCalls.Text.ToLower())) //text boxa ne yazarsan onu bulu önemli!!
+                    {
+                        lstFound.Items.Add(name);
+                    }
+                }
+            }
+        }
     }
 }
